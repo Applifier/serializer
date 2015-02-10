b765dae7fb96e1cd81df77a93282f4f3a4ae00a9
Jaakko Lukkari <jaakko@applifier.com>
2015-02-10 09:42:41 +0200

    More tests

--- serializer_test.go
+++ serializer_test.go
@@ -1,6 +1,9 @@
 package serializer
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 var serializer = NewSecureSerializer([]byte("somesecretkey"), []byte("anothersecretstring"))
 
@@ -30,5 +33,57 @@ func TestSecureSerializerWithInt(t *testing.T) {
 		}
 	} else {
 		t.Error("Returned value has wrong type")
+	}
+}
+
+func TestSecureSerializerWithMap(t *testing.T) {
+	val := make(map[string]interface{})
+	val["foo"] = "bar"
+
+	result, err := serializer.Stringify(val)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	var back interface{}
+
+	err = serializer.Parse(result, &back)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if !reflect.DeepEqual(val, back) {
+		t.Error("Values did not match after Stringify and Parse")
+	}
+
+}
+
+func TestSecureSerializerWithArray(t *testing.T) {
+	var val [2]interface{}
+	val[0] = "bar"
+	val[1] = "foo"
+
+	result, err := serializer.Stringify(val)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	var back [2]interface{}
+
+	err = serializer.Parse(result, &back)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if !reflect.DeepEqual(val, back) {
+		t.Error("Values did not match after Stringify and Parse")
 	}
 }
